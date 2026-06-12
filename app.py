308802fecede17058a5cba649ae5c7573853137e
durgeshsinghh <2k23.csds2312171@gmail.com>
2026-06-12 22:22:07 +0530

    check ci

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
-def sub(a,b):
-    return a-b
 
-def mul(a,b):
-    return a*b
+def sub(a, b):
+    return a - b
 
-print(add(2,3))
-print(sub(5,3))
-print(mul(2,3))
+
+def mul(a, b):
+    return a * b
+
+
+print(add(2, 3))
+print(sub(5, 3))
+print(mul(2, 3))
